9315a5d2e23ad00d29c64aae9cccfd01b23b1e3c
RodriLaziale1616 <edgar.rodriquintana16@gmail.com>
2026-03-14 11:54:08 -0300

    disable export routes to restore backend

--- backend/src/routes/export.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const auth = require('../middlewares/auth');
-const requireRole = require('../middlewares/roles');
-const controller = require('../controllers/export.controller');
-
-router.get('/excel', auth, requireRole('admin'), controller.excel);
-
-module.exports = router;
